62b2eb3392ec63e9ce1ae51175c93924e17c2ad1
jesslauv <jessicalauv@gmail.com>
2022-11-20 12:17:53 -0500

    Moved files to different folder

--- Lectures/week2/Homework2/src/com/yearup/week2/homework/Calculator.java
+++
@@ -1,8 +0,0 @@
-package com.yearup.week2.homework;
-
-public interface Calculator {
-    // all methods are assumed to be public and abstract
-    double calcArea(double length, double width);
-    double calcSum(double val1, double val2);
-    double calcVolume(double length, double width, double height);
-}
